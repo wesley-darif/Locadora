0d8128ad73f872d72ff0ef59587e2a58151db692
wesley-darif <wesleysdarifs@gmail.com>
2026-09-26 00:52:27 -0300

    Ajustando banco com azure

--- banco.py
+++ banco.py
@@ -1,17 +1,34 @@
+import os
 import pyodbc
+from dotenv import load_dotenv
 
+load_dotenv()
 
-dados_conexao = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        "SERVER=localhost\\SQLEXPRESS;"
-        "DATABASE=LOCADORA1;"
-        "Trusted_Connection=yes;"
-        "TrustServerCertificate=yes;"
-)
+servidor = os.getenv("DB_SERVER")
+banco = os.getenv("DB_DATABASE")
+usuario = os.getenv("DB_USER")
+senha = os.getenv("DB_PASSWORD")
+
 try:
-    conexao = pyodbc.connect(dados_conexao)
-    print("Conexao bem sucedida")
-    cursor = conexao.cursor()
-except ValueError:
-    print("Erro ao conectar ao Banco de Dados")
+    conexao = pyodbc.connect(
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER=tcp:{servidor},1433;"
+        f"DATABASE={banco};"
+        f"UID={usuario};"
+        f"PWD={senha};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
 
+    cursor = conexao.cursor()
+
+    print("Conectado ao Azure SQL com sucesso!")
+    cursor.execute("SELECT DB_NAME()")
+    resultado = cursor.fetchone()
+
+    print("Banco conectado:", resultado[0])
+
+except Exception as erro:
+    print("Erro ao conectar ao banco de dados:")
+    print(erro)
